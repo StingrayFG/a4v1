aa76df30d95818fb2f9cd0570c9f927d3098c1e3
Stingray_FG <igorzam2003@gmail.com>
2022-09-15 10:16:34 +0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -11,26 +11,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        aerodynamic_surface ad = new aerodynamic_surface();
-        ad.slope = 5;
-        ad.length = 4;
-        ad.root_offset = 0.5f;
+        //aerodynamic_surface ad = new aerodynamic_surface();
+        //ad.slope = 5;
+        //ad.length = 4;
+        //ad.root_offset = 0.5f;
 
-        ad.root_functions = new lc_dc_stock();
-        ad.tip_functions = new lc_dc_stock();
+        //ad.root_functions = new lc_dc_stock();
+        //ad.tip_functions = new lc_dc_stock();
 
-        ad.sections_main.Add(new ad_surface_section(5, 4.56f, 0, new Point3(6, 0.5f, 0.2f), new Point3(4, 0.5f, 0.2f)));
+        //ad.sections_main.Add(new ad_surface_section(5, 4.56f, 0, new Point3(6, 0.5f, 0.2f), new Point3(4, 0.5f, 0.2f)));
 
-        ad.sections_main.Add(new ad_surface_section(10, 0.4f, 0.5f, new Point3(5, 2f, 0.5f), new Point3(4, 2f, 0.5f)));
+        //ad.sections_main.Add(new ad_surface_section(10, 0.4f, 0.5f, new Point3(5, 2f, 0.5f), new Point3(4, 2f, 0.5f)));
 
-        ad.sections_main.Add(new ad_surface_section(15, 0.4f, 1, new Point3(4, 4.5f, 0.5f), new Point3(3.5f, 4.5f, 0.5f)));
+        //ad.sections_main.Add(new ad_surface_section(15, 0.4f, 1, new Point3(4, 4.5f, 0.5f), new Point3(3.5f, 4.5f, 0.5f)));
 
-        aircraft ac_new = new aircraft();
-        environment env_new = new environment(103250, 290);
+        //aircraft ac_new = new aircraft();
+        //environment env_new = new environment(103250, 290);
 
-        ac_new.surfaces.Add(ad);
+        //ac_new.surfaces.Add(ad);
 
-        ad.calc_initial();
+        //ad.calc_initial();
 
         //float rotationX = 30;
         //float rotationY = 90;
@@ -57,5 +57,8 @@ class Program
 
         ////Console.WriteLine(ad.recalc_sections(5, 15));
 
+        Point3 p1 = new Point3(2, 2, 4);
+        Point3 p2 = new Point3(3, 2, 1);
+        Point3 p3 = p1 + p2;
     }
 }
